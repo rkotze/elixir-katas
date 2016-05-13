0034436a5034f2658b105f6f52e8b4c627382fdc
Richard Kotze <rkotze@dctfh.com>
2016-05-13 00:55:45 +0000

    carry on roman kata

--- lib/roman_numerals/roman_numerals.ex
+++ lib/roman_numerals/roman_numerals.ex
@@ -1,11 +1,20 @@
 defmodule RomanNumerals do
 
+  @numerals [{5,"V"}, {4, "IV"}, {1, "I"}]
+
+  def convert(number) do
+    converts(number, @numerals)
+  end
+
   def convert(number) when number < 1 do
     ""
   end
 
-  def convert(number) do
-    "I" <> convert(number - 1)
+  def convert(number, [{arabic, roman} |tail]) when number >= arabic do
+    roman <> convert(number - arabic, [{arabic, roman} | tail]);
   end
 
+  def convert(number, [head | tail]) do
+    converts(number, tail)
+  end
 end
--- test/roman_numerals/roman_numerals_test.exs
+++ test/roman_numerals/roman_numerals_test.exs
@@ -5,7 +5,10 @@ defmodule RomanNumeralsTest do
     test_cases = [{0, ""},
                   {1, "I"},
                   {2, "II"},
-                  {3, "III"}]
+                  {3, "III"},
+                  {4, "IV"},
+                  {5, "V"},
+                  {6, "VI"}]
 
     Enum.each test_cases, fn ({arabic, roman}) -> assert RomanNumerals.convert(arabic) == roman end;
   end
